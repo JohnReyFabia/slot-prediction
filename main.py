27c789a2ac4b533d7c2cee346c58af8469340bff
JohnReyFabia <201880452@psu.palawan.edu.ph>
2023-11-30 23:34:43 +0800

    login page

---
+++ main.py
@@ -0,0 +1,9 @@
+import sklearn
+import numpy as np
+import pandas as pd
+
+##load data
+
+
+df = pd.read_excel("C:\Users\colou\Desktop\Slot Prediction\No_of_available_slots_2020-2023-24Nov2023.xlsx")
+print(df)
